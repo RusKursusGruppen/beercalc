6ecc121399fc33f780f0b9bfeffca4651aa4119f
Daniel Egeberg <daniel.egeberg@gmail.com>
2011-07-26 18:17:07 +0200

    Produktting.

--- app/controllers/product.py
+++ app/controllers/product.py
@@ -3,7 +3,7 @@ from app.utils.misc import template_response, local, urlfor, redirect
 
 from app.controllers import notfound
 
-from app.document import inventory
+from app.document import inventory, document
 
 def browse():
     products = [(a.id, a.name, a.stock) for a in inventory.list_by_name()]
@@ -11,14 +11,39 @@ def browse():
         products = products,
     )
 
-def view(product_id):
+def edit(product_id):
     try:
         product = inventory.get_product(product_id)
     except KeyError:
         notfound()
         return
 
-    template_response("/page/test.mako", test=product.export())
+    purchases_iter = ((p.name, p.price, p.quantity, p.date) for p in product.purchases)
+
+    template_response("/page/product/edit.mako",
+        id = product.id,
+        name = product.name,
+        stock = product.stock,
+        fixedprice = product.fixedprice,
+        purchases = purchases_iter
+    )
+
+def edit_do(product_id):
+    name = local.request.form.get("name", u"")
+    fixedprice = local.request.form.get("fixedprice", u"")
+    if len(fixedprice) == 0:
+        fixedprice = None
+    else:
+        fixedprice = int(fixedprice)
+
+    account = inventory.get_product(product_id)
+
+    account.name = name
+    account.fixedprice = fixedprice
+
+    document.save(u"Ændrede data for produkt '%s'" % (name,))
+
+    redirect("product.edit", product_id=product_id)
 
 def create_form():
     pass
@@ -26,3 +51,8 @@ def create_form():
 def create_do():
     pass
 
+def purchase_form():
+    pass
+
+def purchase_do():
+    pass
