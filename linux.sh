c6b70b02591027be18493ffa4aebe38df07a59b3
hkb <hans.kristian.bjerregaard@gmail.com>
2011-07-27 23:34:35 +0200

    Added simpler way to run the program on linux systems.

---
+++ linux.sh
@@ -0,0 +1,3 @@
+python beercalc.py &            # starts the program
+sleep 1 &                       # "inteligently" waits for the program to start
+xdg-open http://localhost:5000  # opens the default browser to use the program
